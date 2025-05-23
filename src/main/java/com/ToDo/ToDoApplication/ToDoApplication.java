b60ce32028d2b5475d83dad9fc5d29ef9ca32890
dlb34 <davidlbull@googlemail.com>
2025-05-23 15:48:56 +0100

    encrypted properties

--- src/main/java/com/ToDo/ToDoApplication/ToDoApplication.java
+++ src/main/java/com/ToDo/ToDoApplication/ToDoApplication.java
@@ -1,9 +1,9 @@
 package com.ToDo.ToDoApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableJpaRepositories(basePackages = "com.ToDo.ToDoApplication.repository")
 @EntityScan(basePackages = "com.ToDo.ToDoApplication.model")
-
+@EnableEncryptableProperties
 public class ToDoApplication {
 
 	public static void main(String[] args) {
